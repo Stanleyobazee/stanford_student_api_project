687f3f77cfafef25c10b71b01d95e497262e119b
Stanleyobazee <stanleyobazee@gmail.com>
2025-08-19 14:51:33 +0200

    "Clean up unused imports in health controller test

--- tests/health_controller_test.go
+++ tests/health_controller_test.go
@@ -1,51 +1,17 @@
 package tests
 
 import (
-	"errors"
-	"net/http"
-	"net/http/httptest"
 	"testing"
-
-	"stanford-uni-students-api/controllers"
-
-	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
-type MockDatabase struct {
-	mock.Mock
-}
-
-func (m *MockDatabase) Ping() error {
-	args := m.Called()
-	return args.Error(0)
-}
-
 func TestHealthCheck_Success(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-	
-	logger := logrus.New()
-	logger.SetLevel(logrus.FatalLevel)
-	
-	mockDB := new(MockDatabase)
-	mockDB.On("Ping").Return(nil)
-	
-	// For now, skip this test since we need to refactor the controller
-	// to accept an interface instead of *sql.DB
-	t.Skip("Health controller needs interface refactoring for proper testing")
+	// Skip this test since it requires database connection
+	// TODO: Refactor health controller to use interface for proper testing
+	t.Skip("Health controller test requires database connection - skipping in CI")
 }
 
 func TestHealthCheck_DatabaseError(t *testing.T) {
-	gin.SetMode(gin.TestMode)
-	
-	logger := logrus.New()
-	logger.SetLevel(logrus.FatalLevel)
-	
-	mockDB := new(MockDatabase)
-	mockDB.On("Ping").Return(errors.New("connection failed"))
-	
-	// Skip for same reason as above
-	t.Skip("Health controller needs interface refactoring for proper testing")
+	// Skip this test since it requires database connection
+	// TODO: Refactor health controller to use interface for proper testing
+	t.Skip("Health controller test requires database connection - skipping in CI")
 }
